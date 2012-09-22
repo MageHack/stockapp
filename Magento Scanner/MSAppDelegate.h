0a66e14f973b6126ab7dba40131edcab7ac2d7f5
Red <red@riothq.com>
2012-09-22 10:31:31 +0100

    Initial Commit

---
+++ Magento Scanner/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  Magento Scanner
+//
+//  Created by Red Davis on 22/09/2012.
+//  Copyright (c) 2012 NIfty Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Magento Scanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Magento Scanner
+//
+//  Created by Red Davis on 22/09/2012.
+//  Copyright (c) 2012 NIfty Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
